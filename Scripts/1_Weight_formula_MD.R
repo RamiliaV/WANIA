d05c96e05eed1afa3ec5f3b3fe8e1235ac3a6b68
RamiliaV <mukhamadeeva.r@gmail.com>
2022-04-18 01:27:12 +0300

    Added scripts and results

---
+++ Scripts/1_Weight_formula_MD.R
@@ -0,0 +1,400 @@
+library(tidyverse)
+library(tidygraph)
+library(ggraph)
+library(visNetwork)
+library(igraph)
+library(stringr)
+library(beepr)
+library(rlang)
+library(rstatix)
+library(ggpubr)
+library(ggdist)
+library(scales)
+library(apcluster)
+
+#############################################################
+# 1st function - Создание графов без и по каждому параметру #
+#############################################################
+
+get_networks <- function(nodes, edges, parameters) {
+  
+  graph_list <- list()
+  
+  for (i in 1:(length(parameters))) {
+    
+    if (i == 1) {
+      
+      nodes_local <- nodes %>%
+        dplyr::select(1:7, 11)
+      
+      data.net <- tbl_graph(
+        nodes = nodes_local, 
+        edges = edges,
+        directed = F
+      )
+      
+    } else {
+      
+      nodes_local <- nodes %>%
+        dplyr::select(1:7, 11, all_of(parameters[i]))
+      
+      data.net <- tbl_graph(
+        nodes = nodes_local, 
+        edges = edges,
+        directed = F
+      )
+      
+    }
+    
+    graph_list[[i]] <- data.net
+    
+  }
+  
+  names(graph_list) <- parameters
+  
+  assign(x = "graph_list", value = graph_list, envir = .GlobalEnv)
+  
+}
+
+# get_networks(nodes = nodes, edges = edges, parameters = parameters)
+
+########################################################
+# 2nd function -  Получение параметров и визуализация #
+########################################################
+
+get_network_parameters <- function(graph_list) {
+  
+  # graph parameters
+  parameters <- names(graph_list)
+  
+  graph_parameters <- data.frame(graphs = parameters)
+  graph_parameters$diameter <- NA
+  graph_parameters$mean_distance <- NA
+  
+  parameters_list <- list()
+  graph_graphs <- list()
+  
+  for (i in 1:length(parameters)) {
+    
+    if (i == 1) {
+      
+      graph_parameter_n <- graph_list[[i]] %>%
+        dplyr::mutate(diameter = graph_diameter(directed = F),
+                      mean_distance = graph_mean_dist(directed = F)) %>%
+        as_tibble() %>%
+        dplyr::select(diameter, mean_distance) %>%
+        distinct() %>%
+        unlist()
+      
+      graph_parameters[i, 2:3] <- graph_parameter_n
+      
+      node_parameters <- graph_list[[i]] %>%
+        activate(nodes) %>%
+        mutate(betweenness = centrality_betweenness(),
+               closeness = centrality_closeness(),
+               hub = centrality_hub(),
+               degree = centrality_degree()) %>%
+        as.tibble()
+      
+      graph_plot_c_b_h <- graph_list[[i]] %>%
+        activate(nodes) %>%
+        left_join(node_parameters) %>% 
+        ggraph(layout = "kk") + 
+        geom_edge_link(color = "grey") +
+        geom_node_point(aes(size = closeness, colour = betweenness, alpha = hub)) +
+        geom_node_text(aes(label = label, size = hub), repel = T) +
+        scale_color_gradient(low = "yellow", high = "red") +
+        labs(title = str_c(c(firstup(parameters[i]), ". Diameter=", graph_parameter_n[1], ", Mean distance=", graph_parameter_n[2], "."), collapse = "")) +
+        theme_graph() 
+      
+      nodes_xy <- graph_list[[i]] %>%
+        activate(nodes) %>%
+        as.tibble() %>%
+        select(x,y) 
+      names(nodes_xy) <- c("x1", "y1")
+      
+      graph_plot_c_b_h_2 <- graph_list[[i]] %>%
+        activate(nodes) %>%
+        left_join(node_parameters) %>% 
+        ggraph(layout = nodes_xy) + 
+        geom_edge_link(color = "grey") +
+        geom_node_point(aes(size = closeness, colour = betweenness, alpha = hub)) +
+        geom_node_text(aes(label = label, size = hub), repel = T) +
+        scale_color_gradient(low = "yellow", high = "red") +
+        labs(title = str_c(c(firstup(parameters[i]), ". Diameter=", graph_parameter_n[1], ", Mean distance=", graph_parameter_n[2], "."), collapse = "")) +
+        theme_graph() 
+      
+      graph_graphs[[i*2-1]] <- graph_plot_c_b_h
+      graph_graphs[[i*2]] <- graph_plot_c_b_h_2
+      
+      # ggsave(filename = str_c(firstup(parameters[i]), ".png"), plot = graph_plot_c_b_h, scale = 3)
+      
+    } else {
+      
+      node_info <- graph_list[[i]] %>%
+        activate(nodes) %>%
+        as_tibble() %>%
+        select(1, 9)
+      
+      node_pre_parameters <- graph_list[[i]] %>%
+        activate(edges) %>%
+        left_join(node_info, by = c("from" = "id")) %>%
+        left_join(node_info, by = c("to" = "id"), suffix = c("_from", "_to")) %>%
+        mutate(new_weight = !!sym(str_c(parameters[i], "_from")) + !!sym(str_c(parameters[i], "_to"))) %>%
+        mutate(new_weight = 2**((new_weight - min(new_weight) / diff(range(new_weight)))) * weight)
+      
+      graph_parameter_n <- node_pre_parameters %>%
+        dplyr::mutate(diameter = graph_diameter(weights = new_weight, directed = F),
+                      mean_distance = graph_mean_dist(directed = F)) %>%
+        as.tibble() %>%
+        dplyr::select(diameter, mean_distance) %>%
+        distinct() %>%
+        unlist()
+      
+      graph_parameters[i, 2:3] <- graph_parameter_n
+      
+      node_parameters <- node_pre_parameters %>%
+        activate(nodes) %>%
+        mutate(betweenness = centrality_betweenness(weights = new_weight),
+               closeness = centrality_closeness(weights = new_weight),
+               hub = centrality_hub(weights = new_weight),
+               degree = centrality_degree(weights = new_weight)) %>%
+        as.tibble()
+      
+      graph_plot_c_b_h <- node_pre_parameters %>%
+        activate(nodes) %>%
+        left_join(node_parameters) %>% 
+        ggraph(layout = "nicely") + 
+        geom_edge_link(aes(alpha = new_weight), color = "grey") +
+        geom_node_point(aes(size = closeness, colour = betweenness, alpha = hub)) +
+        geom_node_text(aes(label = label, size = hub), repel = T) +
+        scale_color_gradient(low = "yellow", high = "red") +
+        labs(title = str_c(c(firstup(parameters[i]), ". Diameter=", graph_parameter_n[1], ", Mean distance=", graph_parameter_n[2], "."), collapse = "")) +
+        theme_graph()
+      
+      nodes_xy <- graph_list[[i]] %>%
+        activate(nodes) %>%
+        as.tibble() %>%
+        select(x,y) 
+      names(nodes_xy) <- c("x1", "y1")
+      
+      graph_plot_c_b_h_2 <- graph_list[[i]] %>%
+        activate(nodes) %>%
+        left_join(node_parameters) %>% 
+        ggraph(layout = nodes_xy) + 
+        geom_edge_link(color = "grey") +
+        geom_node_point(aes(size = closeness, colour = betweenness, alpha = hub)) +
+        geom_node_text(aes(label = label, size = hub), repel = T) +
+        scale_color_gradient(low = "yellow", high = "red") +
+        labs(title = str_c(c(firstup(parameters[i]), ". Diameter=", graph_parameter_n[1], ", Mean distance=", graph_parameter_n[2], "."), collapse = "")) +
+        theme_graph() 
+      
+      graph_graphs[[i*2-1]] <- graph_plot_c_b_h
+      graph_graphs[[i*2]] <- graph_plot_c_b_h_2     
+      
+      # ggsave(filename = "graph_plot_try.png", plot = graph_plot_c_b_h, scale = 3)
+      
+    }
+    
+    parameters_list[[i]] <- node_parameters
+    
+  }
+  
+  assign(x = "graph_parameters", value = graph_parameters, envir = .GlobalEnv)
+  
+  names(parameters_list) <- parameters
+  assign(x = "parameter_list", value = parameters_list, envir = .GlobalEnv)
+  
+  names(graph_graphs) <- str_c(rep(parameters, each=2), c("_kk", "_xy"))
+  assign(x = "plot_list", value = graph_graphs, envir = .GlobalEnv)
+  
+}
+
+# get_network_parameters(graph_list_)
+
+#############################################
+# 3rd function - Сравнение параметров нодов #
+#############################################
+
+node_parameters_names <- c("betweenness","closeness","hub", "degree")
+
+compare_node_parameters <- function(parameter_list) {
+  
+  comparison_node_parameters <- list()
+  
+  for (i in 1:length(node_parameters_names)) {
+    
+    parameter_table <- data.frame(matrix(nrow = nrow(parameter_list[[i]]),ncol = length(parameters)))
+    names(parameter_table) <- parameters
+    
+    for (j in 1:length(parameters)) {
+      node_parameter_j <- parameter_list[[j]] %>%
+        select(node_parameters_names[i]) %>%
+        unlist()
+      parameter_table[,j] <- node_parameter_j
+    }
+    
+    parameter_table_pre_plot <- parameter_table %>%
+      pivot_longer(everything(), names_to = "parameter", values_to = "node_parameter")
+    
+    parameter_table_pre_plot$parameter <- factor(parameter_table_pre_plot$parameter, levels = names(parameter_list))
+    
+    kruskal <- parameter_table_pre_plot %>%
+      kruskal_test(node_parameter ~ parameter)
+    
+    comparison_node_parameters[[i*3-2]] <- kruskal
+    
+    pwc <- parameter_table_pre_plot %>%
+      pairwise_wilcox_test(node_parameter ~ parameter) %>%
+      add_xy_position(x = "parameter") %>%
+      filter(p.adj.signif != "ns") %>%
+      filter(group1 == "default") # ONLY COMPARISON WITH DEFAULT GRAPH (NO WEIGHT)
+    
+    comparison_node_parameters[[i*3-1]] <- pwc
+    
+    boxplot <-
+      ggplot(
+        parameter_table_pre_plot,
+        aes(
+          x = parameter,
+          y = node_parameter
+        )
+      ) +
+      geom_boxplot(width = .12,
+                   ## remove outliers
+                   # outlier.color = NA,
+                   alpha = 0.5) +
+      coord_flip() +
+      stat_pvalue_manual(pwc, hide.ns = F, coord.flip = T, step.increase = 0.1, ) +
+      labs(
+        title = firstup(node_parameters_names[i]),
+        subtitle = get_test_label(kruskal, detailed = TRUE),
+        caption = get_pwc_label(pwc),
+        x = "",
+        y = ""
+      ) +
+      theme_pubr() +
+      scale_y_log10(breaks = trans_breaks("log10", function(x) 10^x),
+                    labels = trans_format("log10", math_format(10^.x))) +
+      annotation_logticks(sides="b")
+    
+    comparison_node_parameters[[i*3]] <- boxplot
+    
+  }
+  
+  names(comparison_node_parameters) <- str_c(rep(node_parameters_names, each=3), c("_kruskal", "_wilcox_pwc", "_rainplot"))
+  assign(x = "comparison_node_parameters", value = comparison_node_parameters, envir = .GlobalEnv)
+  
+}
+
+# compare_node_parameters(parameter_list)
+
+######################################
+# 4th function - Составление формулы #
+######################################
+
+coef_table <- data.frame(p = c("*", "**", "***", "****"), coef_part = c(0.125,0.25,0.5,1))
+
+stating_parameter_formula <- function(comparison_node_parameters) {
+  
+  wilcox_pwc_list <- comparison_node_parameters[str_detect(names(comparison_node_parameters),"wilcox")]
+  
+  wilcox_join_table <- wilcox_pwc_list %>%
+    bind_rows(.id = "node_parameter") %>%
+    select(node_parameter, group1, group2, p.adj, p.adj.signif)
+  
+  formula_coef <- wilcox_join_table %>%
+    left_join(coef_table, by = c("p.adj.signif" = "p")) %>%
+    group_by(group2) %>%
+    summarise(coef = sum(coef_part)) %>%
+    column_to_rownames("group2")
+  
+  assign(x = "formula_coef", value = formula_coef, envir = .GlobalEnv)
+  
+}
+
+# stating_parameter_formula(comparison_node_parameters)
+
+##############################################
+# 5th function - Построение графа с формулой #
+##############################################
+
+graph_with_formula <- function(nodes, edges, formula_coef, name) {
+  
+  graph_list <- list()
+  
+  graph <- tbl_graph(
+    nodes = nodes, 
+    edges = edges,
+    directed = F
+  )
+  
+  node_info <- graph %>%
+    activate(nodes) %>%
+    as_tibble() %>%
+    mutate(coef = conservative * formula_coef["conservative", 1] +
+             # charge * formula_coef["charge", 1] +
+             hbonds * formula_coef["hbonds", 1] + 
+             # hydrophobicity * formula_coef["hydrophobicity", 1] +
+             ramachadran * formula_coef["ramachadran", 1] + 
+             RMSF * formula_coef["RMSF", 1]) %>%
+    mutate(coef = scale(coef, center = F)) %>%
+    select(1, last_col())
+  
+  node_pre_parameters <- graph %>%
+    activate(edges) %>%
+    left_join(node_info, by = c("from" = "id")) %>%
+    left_join(node_info, by = c("to" = "id"), suffix = c("_from", "_to")) %>%
+    mutate(new_weight = scale(coef_from + coef_to, center = F) * weight * five_aa)
+  
+  node_parameters <- node_pre_parameters %>%
+    activate(nodes) %>%
+    left_join(node_info, by = "id") %>%
+    mutate(betweenness = centrality_betweenness(weights = new_weight),
+           hub = centrality_hub(weights = new_weight),
+           degree = centrality_degree(weights = new_weight)) %>%
+    as_tibble()
+  
+  graph_pre_plot <- node_pre_parameters %>%
+    activate(nodes) %>%
+    left_join(node_parameters)
+  
+  graph_list[[1]] <- graph_pre_plot
+  
+  graph_plot_c_b_h <- graph_pre_plot %>%
+    ggraph(layout = "graphopt") + 
+    geom_edge_link(color = "grey") +
+    geom_node_point(aes(colour = betweenness, size = hub, alpha = degree)) +
+    geom_node_text(aes(label = label, size = hub), repel = T) +
+    scale_color_gradient(low = "yellow", high = "red") +
+    scale_alpha_continuous(range = c(0.5,1)) +
+    scale_size_continuous(trans = "log10") +
+    theme_graph()
+  
+  # ggsave(filename = "graph_plot_c_b_h.png", plot = graph_plot_c_b_h, scale = 4)
+  graph_list[[2]] <- graph_plot_c_b_h
+  
+  nodes_xy <- graph %>%
+    activate(nodes) %>%
+    as.tibble() %>%
+    select(x,y) 
+  names(nodes_xy) <- c("x1", "y1")
+  
+  graph_plot_c_b_h_2 <- graph_pre_plot %>%
+    ggraph(layout = nodes_xy) + 
+    geom_edge_link(color = "grey") +
+    geom_node_point(aes(colour = betweenness, size = hub, alpha = degree)) +
+    geom_node_text(aes(label = label, size = hub), repel = T) +
+    scale_color_gradient(low = "yellow", high = "red") +
+    scale_alpha_continuous(range = c(0.5,1)) +
+    scale_size_continuous(trans = "log10") +
+    theme_graph() 
+  
+  # ggsave(filename = "graph_plot_c_b_h_2.png", plot = graph_plot_c_b_h_2, scale = 4)
+  graph_list[[3]] <- graph_plot_c_b_h_2
+  
+  names(graph_list) <- c("graph", "plot_layout", "plot_coord")
+  
+  assign(x = str_c("graph_list_w-plots", name, sep = "_"), value = graph_list, envir = .GlobalEnv)
+  
+}
---
+++ Scripts/2_Comparative_analysis.R
@@ -0,0 +1,191 @@
+library(tidyverse)
+library(tidygraph)
+library(ggraph)
+library(visNetwork)
+library(igraph)
+library(stringr)
+library(beepr)
+library(rlang)
+library(rstatix)
+library(ggpubr)
+library(ggdist)
+library(scales)
+library(apcluster)
+
+####################
+# Сравнение графов #
+####################
+
+compare_graphs_with_formula <- function(graph_1, graph_2, names) {
+  
+  # FIXME to remove after
+  # graph_1 <- `graph_list_w-plots_3136192901`[[1]]
+  # graph_2 <- `graph_list_w-plots_3136217149`[[1]]
+  
+  comparison_graphs_list <- list()
+  
+  nodes_1 <- graph_1 %>%
+    activate(nodes) %>%
+    as_tibble()
+  
+  nodes_2 <- graph_2 %>%
+    activate(nodes) %>%
+    as_tibble() %>%
+    mutate(id = id + nrow(nodes_1))
+  
+  diff <- nodes_1 %>%
+    full_join(nodes_2, by = "label") %>%
+    as_tibble() %>%
+    mutate(diff = abs(coef.x - coef.y)) %>%
+    pull(diff)
+  
+  nodes <- nodes_1 %>%
+    bind_rows(nodes_2, .id = "graph") %>%
+    mutate(graph = ifelse(graph == 1, names[1], names[2])) %>%
+    mutate(diff = rep(diff, 2))
+  
+  edges_1 <- graph_1 %>%
+    activate(edges) %>%
+    as_tibble()
+  
+  edges_2 <- graph_2 %>%
+    activate(edges) %>%
+    as_tibble()
+  
+  # edges_joined <- edges_1 %>%
+  #   full_join(edges_2, by = c("from", "to")) %>%
+  #   mutate(join_weight = ifelse(!is.na(new_weight.x)&!is.na(new_weight.y),
+  #                               new_weight.x + new_weight.y,
+  #                                ifelse(is.na(new_weight.x), new_weight.y, new_weight.x))) %>%
+  #   distinct() %>%
+  #   select(from, to, new_weight.x, new_weight.y, join_weight) %>%
+  #   mutate(graph_type = ifelse(is.na(new_weight.x)|is.na(new_weight.y), TRUE, FALSE)) %>%
+  #   select(-new_weight.x, -new_weight.y)
+  
+  edges_joined <- edges_1 %>%
+    full_join(edges_2, by = c("from", "to")) %>%
+    mutate(join_weight = ifelse(!is.na(new_weight.x)&!is.na(new_weight.y),
+                                abs(new_weight.x - new_weight.y), 0)) %>%
+    distinct() %>%
+    select(from, to, new_weight.x, new_weight.y, join_weight) %>%
+    mutate(graph_type = ifelse(is.na(new_weight.x)|is.na(new_weight.y), TRUE, FALSE)) %>%
+    select(-new_weight.x, -new_weight.y)
+  
+  edges_1 <- edges_1 %>%
+    left_join(edges_joined, by = c("from", "to")) %>%
+    distinct() 
+  edges_2 <- edges_2 %>%
+    left_join(edges_joined, by = c("from", "to")) %>%
+    mutate(from = from + nrow(nodes_1), to = to + nrow(nodes_1)) %>%
+    distinct()
+  edges <- edges_1 %>%
+    bind_rows(edges_2, .id = "graph") %>%
+    mutate(graph = ifelse(graph == 1, names[1], names[2]))
+  
+  graph <- tbl_graph(
+    nodes = nodes, 
+    edges = edges,
+    directed = F
+  )
+  
+  comparison_graphs_list[[1]] <- graph
+  
+  nodes_xy <- graph %>%
+    activate(nodes) %>%
+    as.tibble() %>%
+    select(x,y) 
+  names(nodes_xy) <- c("x1", "y1")
+  
+  graph_1a <- tbl_graph(
+    nodes = nodes_1, 
+    edges = edges_1,
+    directed = F
+  )
+  
+  edges_2 <- edges_2 %>%
+    mutate(from = from - nrow(nodes_1), to = to - nrow(nodes_1))
+  
+  graph_2a <- tbl_graph(
+    nodes = nodes_2, 
+    edges = edges_2,
+    directed = F
+  )
+  
+  node1 <- V(graph_1a)$label
+  node2 <- V(graph_2a)$label
+  node_index <- c(node1,node2)
+  
+  matrix_1 <- as.matrix(as_adj(graph_1a, names = T)) * as.numeric(nodes_1$coef)
+  matrix_2 <- as.matrix(as_adj(graph_2a, names = T)) * as.numeric(nodes_2$coef)
+  
+  all_net_matrix <- abs(matrix_1 - matrix_2)
+  
+  comparison_graphs_list[[2]] <- all_net_matrix
+  
+  # cluster_scores <- list()
+  # sim_scores <- c()
+  # 
+  # for (i in 1:1000) {
+  # 
+  #   clusters <- apcluster(all_net_matrix)
+  # 
+  #   # calc_cluster_score <- function(cluster){
+  #   #   LS1 <- sapply(cluster,function(x)median(matrix_1[x, cluster])) %>% sum
+  #   #   LS2 <- sapply(cluster,function(x)median(matrix_2[x, cluster])) %>% sum
+  #   #   LS <- median(LS1, LS2)
+  #   #   return(LS)
+  #   # }
+  # 
+  #   calc_cluster_score <- function(cluster){
+  #     LS <- sapply(cluster,function(x) max(all_net_matrix[x, cluster])) %>% mean()
+  #     return(LS)
+  #   }
+  # 
+  #   score <- sapply(1:length(clusters),function(x)1-calc_cluster_score(clusters[[x]]))
+  #   cluster_score <- data.frame(score = score, cluster = sapply(clusters@clusters, function(x) paste0(x, collapse = " ")))
+  # 
+  #   cluster_scores[[i]] <- cluster_score
+  # 
+  #   sim_score <- median(cluster_score$score)
+  #   sim_scores[i] <- sim_score
+  # 
+  # }
+  # 
+  # sim_score_m <- median(sim_scores, na.rm = T)
+  # 
+  # comparison_graphs_list[[3]] <- cluster_scores
+  # comparison_graphs_list[[4]] <- sim_scores
+  # comparison_graphs_list[[5]] <- sim_score_m
+  
+  diff_95 <- graph %>%
+    activate(nodes) %>%
+    as_tibble() %>%
+    summarise(q95 = quantile(diff, .95)) %>%
+    pull()
+  
+  graph_plot_g <- graph %>%
+    activate(nodes) %>%
+    mutate(label = ifelse(diff > diff_95, label, NA)) %>%
+    mutate(coef_2 = ifelse(diff > diff_95, coef*2, coef),
+           diff = ifelse(diff > diff_95, 1, 1))
+  
+  graph_plot <- graph_plot_g %>%
+    ggraph(layout = nodes_xy) + 
+    geom_edge_link(aes(color = graph, alpha = new_weight, linetype = graph_type), width = 1.5) +
+    geom_node_point(aes(color = graph, size = coef_2, alpha = diff)) +
+    geom_node_text(aes(label = label, size = coef_2), repel = T) +
+    scale_edge_linetype_manual(values=c("solid", "dotdash")) +
+    scale_color_manual(values = c("red", "blue")) +
+    scale_size_continuous(trans = "log10") +
+    facet_nodes(~ graph) +
+    theme_graph() +
+    theme(plot.title = element_text(hjust = 0.5))
+  
+  comparison_graphs_list[[3]] <- graph_plot
+  # comparison_graphs_list[[6]] <- graph_plot
+  
+  ggsave(filename = str_c("comparison_graphs_list", str_c(names, collapse = ", "), "_2.png", sep = "_"), plot = graph_plot, scale = 3)
+  
+  assign(x = str_c("comparison_graphs_list", str_c(names, collapse = ", "), sep = "_"), value = comparison_graphs_list, envir = .GlobalEnv)
+  
+}  
